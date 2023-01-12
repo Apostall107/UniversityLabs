3612828568638d34476d3919017d168019063dbd
Oleksandr Hryn <apostalls107@gmail.com>
2023-01-12 12:51:57 +0200

    Lab5 uncommented code

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -11,48 +11,48 @@ namespace Lab5 {
 
 
 
-            ///*--------------------------------------------1 PROBLEM---------------------------------*/
-            //Console.WriteLine("1 problem");
-            //Console.WriteLine("begin");
+            /*--------------------------------------------1 PROBLEM---------------------------------*/
+            Console.WriteLine("1 problem");
+            Console.WriteLine("begin");
 
 
-            //Box box1 = new Box(2, 3, 4);
-            //Box box2 = new Box(1.3, 1, 6);
-            //Console.WriteLine("Surface Area – " + box1.CalculateSurfaceArea());
-            //Console.WriteLine("Lateral Surface Area - " + box1.CalculateLateralSurface());
-            //Console.WriteLine("Volume –  " +box1.CalculateVolume());
+            Box box1 = new Box(2, 3, 4);
+            Box box2 = new Box(1.3, 1, 6);
+            Console.WriteLine("Surface Area – " + box1.CalculateSurfaceArea());
+            Console.WriteLine("Lateral Surface Area - " + box1.CalculateLateralSurface());
+            Console.WriteLine("Volume –  " + box1.CalculateVolume());
 
-            //Console.WriteLine("Surface Area – " + box2.CalculateSurfaceArea().ToString("#0.00"));
-            //Console.WriteLine("Lateral Surface Area - " + box2.CalculateLateralSurface().ToString("#0.00"));
-            //Console.WriteLine("Volume – " + box2.CalculateVolume().ToString("#0.00"));
-
-
-            //Console.WriteLine("end");
-            ///*--------------------------------------------2 PROBLEM---------------------------------*/
-            //Console.WriteLine("2 problem");
-            //Console.WriteLine("begin");
-
-            //Box box3 = new Box(2, -1, 4);
-
-            //Console.WriteLine("end");
-            ///*--------------------------------------------3 PROBLEM---------------------------------*/
-            //Console.WriteLine("3 problem");
-            //Console.WriteLine("begin");
-
-            //Chicken chicken = new Chicken("Mara", 10);
-            //Console.WriteLine(chicken.ProductPerDay);
-            //Chicken chicken2 = new Chicken("Mara", 17);
+            Console.WriteLine("Surface Area – " + box2.CalculateSurfaceArea().ToString("#0.00"));
+            Console.WriteLine("Lateral Surface Area - " + box2.CalculateLateralSurface().ToString("#0.00"));
+            Console.WriteLine("Volume – " + box2.CalculateVolume().ToString("#0.00"));
 
 
-            
-            //Console.WriteLine("end");
-            ///*--------------------------------------------4 PROBLEM---------------------------------*/
-            //Console.WriteLine("4 problem");
-            //Console.WriteLine("begin");
+            Console.WriteLine("end");
+            /*--------------------------------------------2 PROBLEM---------------------------------*/
+            Console.WriteLine("2 problem");
+            Console.WriteLine("begin");
 
-            //Person.Shoping();
+            Box box3 = new Box(2, -1, 4);
 
-            //Console.WriteLine("\nend");
+            Console.WriteLine("end");
+            /*--------------------------------------------3 PROBLEM---------------------------------*/
+            Console.WriteLine("3 problem");
+            Console.WriteLine("begin");
+
+            Chicken chicken = new Chicken("Mara", 10);
+            Console.WriteLine(chicken.ProductPerDay);
+            Chicken chicken2 = new Chicken("Mara", 17);
+
+
+
+            Console.WriteLine("end");
+            /*--------------------------------------------4 PROBLEM---------------------------------*/
+            Console.WriteLine("4 problem");
+            Console.WriteLine("begin");
+
+            Person.Shoping();
+
+            Console.WriteLine("\nend");
             /*--------------------------------------------5 PROBLEM---------------------------------*/
             Console.WriteLine("5 problem");
             Console.WriteLine("begin");
